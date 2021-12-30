11ed79c7ca82eea8a86a5a029e36f1544a395ea4
wzg15801407309 <1051084301@qq.com>
2021-12-30 14:33:33 +0800

    http-git

--- node-base/02-tools/04-buildin-moudle/03-http/01-get/server-cross-domain.js
+++ node-base/02-tools/04-buildin-moudle/03-http/01-get/server-cross-domain.js
@@ -1,21 +1,11 @@
 const logger = require('../../utils/log');
 
 const http = require('http');
+const https = require('https');
 const querystring = require('querystring');
 
 const server = http.createServer((request,response)=>{
-  // // logger.debug(request);
-  // // logger.debug(response);
-  // // 反数据
-  // // response.write('第一次使用node的http核心模块反数据');
-  // response.writeHead('200',{
-  //   // 'content-type':'text/plain'  // 默认是 text/html，
-  //   'content-type':'application/json;charset=utf-8s'  // 默认是 text/html，
-  // })
-  // // response.write('<div>home</div>');
-  // response.write('<div>home</div>');
-  // response.end();
-  // 1230
+
   let data = '';
   request.on('data',(chunk)=>{
     data += chunk;
