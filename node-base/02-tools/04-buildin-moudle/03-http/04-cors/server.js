83b9769be0a1a1c351326c6500e3ee5d646dc25b
wzg15801407309 <1051084301@qq.com>
2022-06-07 11:16:30 +0800

    jonp

---
+++ node-base/02-tools/04-buildin-moudle/03-http/04-cors/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url');
+const server = http.createServer((req,res)=>{
+  let urlstr = req.url;
+  /**知道：这里在函数pare中加个参数的作用是把url对像中的query的值变为一个对像，来使用 */
+  let urlobj = url.parse(urlstr,true);
+  console.log(urlobj);
+  switch(urlobj.pathname){
+    case '/api/data':
+      res.write(`${urlobj.query.cd}('hello')`);
+      break;
+    default:
+      res.write('page not find.');
+  }
+  res.end();
+});
+server.listen('8080',()=>{
+  console.log('http://localhost:8080/');
+});
