1ccac2587811b403af844709aa36482d6d3af97b
wzg15801407309 <1051084301@qq.com>
2022-06-09 11:33:25 +0800

    sider

--- node-base/02-tools/04-buildin-moudle/03-http/06-spider/server.js
+++ node-base/02-tools/04-buildin-moudle/03-http/06-spider/server.js
@@ -1,7 +1,28 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
+const https= require('https');
+const cheerio = require('cheerio');
 
+const filterData = data =>{
+  // console.log('filterData',data);
+  const $ = cheerio.load(data);
+  $('.gl-item .item-desc').each((index,el)=>{
+    console.log(index,$(el).text());
+  })
+  /** 拿回来的数据是一个html 最好的方式就是转化为虚拟的dom来操作 插件 cheerio npm */
+}
+const server = http.createServer((req,res)=>{
+  let data = '';
+  https.get('https://lists.meizu.com',result=>{
+    result.on('data',chunk=>{
+      data+=chunk;
+    });
+    result.on('end',()=>{
+      filterData(data);
+    });
+
+  });
 });
 server.listen('8080',()=>{
   console.log('http://localhost:8080/');
 });
+// 这个就是简单的爬虫
